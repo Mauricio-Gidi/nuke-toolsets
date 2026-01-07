6859b9351af3c46bee7be621aee1e64a370ef5a7
Mauricio Gidi <mgidi0098@gmail.com>
2026-01-07 16:58:16 -0600

    Add files via upload

---
+++ toolsets/toolsets/__init__.py
@@ -0,0 +1,9 @@
+
+"""
+Package initializer for the toolsets package.
+
+This file marks the directory as a Python package and can be used for package-wide
+initialization if needed in the future.
+"""
+
+# No package-wide initialization required at this time.
