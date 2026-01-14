8bed7fc0e670735c4acfb154e1cf7cc95133ef6e
Mauricio Gidi <mgidi0098@gmail.com>
2026-01-14 11:45:33 -0600

    Delete toolsets directory

--- toolsets/mvc/__init__.py
+++
@@ -1,8 +0,0 @@
-
-"""
-Package initializer for the toolsets.mvc subpackage.
-
-This file marks the directory as a Python package for the MVC components of toolsets.
-"""
-
-# No subpackage-wide initialization required at this time.
