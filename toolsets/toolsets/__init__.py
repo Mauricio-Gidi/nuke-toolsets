03cc5fc11f3847ab0098d889715ed7ee66a33bf2
Mauricio Gidi <mgidi0098@gmail.com>
2026-01-22 11:35:24 -0600

    Delete toolsets directory

--- toolsets/toolsets/__init__.py
+++
@@ -1,9 +0,0 @@
-
-"""
-Package initializer for the toolsets package.
-
-This file marks the directory as a Python package and can be used for package-wide
-initialization if needed in the future.
-"""
-
-# No package-wide initialization required at this time.
