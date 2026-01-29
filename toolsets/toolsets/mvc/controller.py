3432f0e2b502dcfecf565070b5f5eb896d6bfb08
Mauricio Gidi <mgidi0098@gmail.com>
2026-01-29 10:19:35 -0600

    Update controller.py

--- toolsets/toolsets/mvc/controller.py
+++ toolsets/toolsets/mvc/controller.py
@@ -363,7 +363,7 @@ class Controller:
         prefix = f"{toolset.user}/{toolset.name}:"
         self.model.toolsets_loader._warnings = [
             w for w in self.model.toolsets_loader._warnings
-            if not (w.startswith(prefix) and ("Missing data.json" in w.lower() or "Invalid data.json" in w.lower()))
+            if not (w.startswith(prefix) and ("missing data.json" in w.lower() or "invalid data.json" in w.lower()))
         ]
         self.update_warnings_button()
 
